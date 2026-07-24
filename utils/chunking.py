c20ba433292546e590a50418be9e4b139917fa0a
Mudassir Rehman <mudassirrehman2805@gmail.com>
2026-07-24 14:57:47 +0530

    Implement basic line-based chunking

--- utils/chunking.py
+++ utils/chunking.py
@@ -1,4 +1,14 @@
 def chunk_text(text):
     lines = text.splitlines()
 
-    print(lines)
+    chunk_size = 2
+
+    for i in range(0, len(lines), chunk_size):
+
+        chunk = lines[i:i + chunk_size]
+
+        print("----- Chunk -----")
+
+        print("\n".join(chunk))
+
+        print()
