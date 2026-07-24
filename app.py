b3895ed619c8ed3e6ad30ef7d0375a7b7f684e11
Mudassir Rehman <mudassirrehman2805@gmail.com>
2026-07-24 12:29:30 +0530

    now it can print .txt file

--- app.py
+++ app.py
@@ -6,4 +6,9 @@ SUPPORTED_FILES = [".pdf", ".txt", ".docx"]
 
 for file in documents_path.iterdir():
     if file.suffix in SUPPORTED_FILES:
-        print(file.name)
+
+        print(f"\n----- {file.name} -----")
+
+        if file.suffix == ".txt":
+            content = file.read_text()
+            print(content)
